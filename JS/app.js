e4bb5fadb2452a95e2511309e76b9c6199b577c0
sangwa7 <didiersangwa@gmail.com>
2022-03-27 12:45:21 -0600

    Add booklist with javascript functionality

--- JS/app.js
+++ JS/app.js
@@ -5,6 +5,8 @@ const button = document.querySelector('#addBook');
 
 const bookList = document.querySelector('#bookList');
 
+const bookData = [{title: 'Tears', author: 'Musoni'}];
+
 button.addEventListener('click', () => {
   if ((titleInput.value !== '') && (authorInput.value !== '')) {
     const bookListRow = document.createElement('tr');
@@ -15,10 +17,22 @@ button.addEventListener('click', () => {
     newAuthor.innerHTML = authorInput.value;
     bookListRow.appendChild(newAuthor);
     const removeButton = document.createElement('button');
-    removeButton.id ='removeMe';
-    console.log('remove me');
+    removeButton.id = 'removeMe';
+    // console.log('remove me');
     removeButton.innerHTML = 'Remove';
     bookListRow.appendChild(removeButton);
     bookList.appendChild(bookListRow);
+
+    bookData.push({title: titleInput.value, author: authorInput.value});
+    console.log(bookData);
+    removeButton.addEventListener('click', () => {
+    // console.log('clicked remove button');
+      bookListRow.remove('tr');
+      console.log(bookData.title);
+      // delete(bookData.title);
+      // console.log(bookData);
+    });
   }
 });
+
+console.log(bookData);
